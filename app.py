340632ac0f79d6cb980445d69bd662e2557d03b3
Piyush Bendale <thetechnolover7@gmail.com>
2019-11-09 11:23:41 +0000

    Server code

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS, cross_origin
-from match import match
+from match import match, encode
+import requests
 
 app = Flask(__name__)
 cors = CORS(app, resources={"/": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/employee/register", methods=["POST", "GET"])
+@cross_origin()
+def encodeImg():
+    if "profileId" in request.form:
+        file = request.files["file"]
+        file.save("tmp/tmp.jpg")
+        if encode(request.form["profileId"]) == True:
+            return jsonify({"status": "ok"}), 200
+    return jsonify({"status": "error"}), 400
 
 @app.route("/", methods=["POST", "GET"])
 @cross_origin()
@@ -12,12 +23,12 @@ def upload():
     if "id" in request.args:
         file = request.files["file"]
         file.save("uploads/tmp.jpg")
-
         if match(request.args.get("id")) == True:
             return jsonify({"auth": "true"}), 200
         else:
             return jsonify({"auth": "false"}), 200
     return jsonify({"status": "error"}), 400
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run()
