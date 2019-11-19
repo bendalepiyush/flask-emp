12516893a62aa3c834a2a1c8aa2b738a0eeffdc4
Dhairyasheel Sutar <34542512+dhairyasheelsutar@users.noreply.github.com>
2019-11-19 08:10:33 +0000

    encode function

--- match.py
+++ match.py
@@ -1,12 +1,19 @@
 import face_recognition
-import pickle
+import joblib
 
 def match(id):
-    known_encoding = pickle.load(open("models/" + id + ".pkl", "rb"))
+    known_encoding = joblib.load("models/" + id + ".pkl")
     unknown_image = face_recognition.load_image_file("uploads/tmp.jpg")
     unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
     results = face_recognition.compare_faces([known_encoding], unknown_encoding)
     print(results)
     return results[0]
+
+def encode(id):
+    known_image = face_recognition.load_image_file("tmp/tmp.jpg")
+    known_encoding = face_recognition.face_encodings(known_image)[0]
+    joblib.dump(known_encoding, "models/" + id + ".pkl")
+    return True
+
 
 
